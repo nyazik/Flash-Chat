1693698e184d092a143a55c679d7ff906711d51d
nyazik <nyaziktenisistka@mail.ru>
2020-10-26 18:47:21 +0300

    Initial Commit

---
+++ Flash Chat iOS13/Models/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Nazik on 19.10.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Messages {
+    let sender :String
+    let body :String
+}
